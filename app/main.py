ac5c5afaa5ae31e427affdc5fe030b7bfb5e9811
Alexandre <lexander.alfa@gmail.com>
2022-11-04 22:06:18 -0300

    incluso static files

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from fastapi.requests import Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI()
 
+app.mount('/static', StaticFiles(directory='static'), "static")
 templantes = Jinja2Templates(directory="templates")
 
 @app.get("/")
